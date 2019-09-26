78a66ceecad95305999b7da00964a35b93ef1cec
Jaswinder Rattanpal <jrattanpal@gmail.com>
2019-09-26 15:55:41 -0400

    Create PkGInstall brnach for ADK Testing

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,7 @@
-sfdx force:org:create -a invmgrscratchorg1 -s -f config/project-scratch-def.json 
+sfdx force:org:create -a invmgrscratchorgpkgtest -s -f config/project-scratch-def.json -d 7
 
-sfdx force:source:push
+sfdx force:package:install --package 04t4T000001dHuU -w 20 
 
 sfdx force:user:permset:assign -n  Inventory_Management_full
 
-sfdx force:org:open
+sfdx force:org:open p /lightning/n/invmgrnp__Inventory_Manager
