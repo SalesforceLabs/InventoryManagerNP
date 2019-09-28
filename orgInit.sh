430ddfbe3dff2c31d3a3784d4f3c1577474cef28
Jaswinder Rattanpal <jrattanpal@gmail.com>
2019-09-28 11:43:08 -0400

    Finish the setup by auto assigning user to location via Apex

--- orgInit.sh
+++ orgInit.sh
@@ -6,4 +6,6 @@ sfdx force:user:permset:assign -n  Inventory_Management_full
 
 sfdx force:data:tree:import -p data/Plan.json
 
-sfdx force:org:open -p /lightning/o/invmgrnp__Location__c/list
+sfdx force:apex:execute -f config/create-demo-data-setup.apex
+
+sfdx force:org:open -p /lightning/page/home
