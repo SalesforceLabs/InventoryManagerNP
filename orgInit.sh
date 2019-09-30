c09d8544385978f9b2e438081877239d046a28f7
Jaswinder Rattanpal <jrattanpal@gmail.com>
2019-09-30 10:14:44 -0400

    Update package version

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,6 @@
 sfdx force:org:create -a invmgrscratchorgpkgtest -s -f config/project-scratch-def.json -d 7
 
-sfdx force:package:install --package 04t4T000001dMcB -w 20 -s AllUsers
+sfdx force:package:install --package 04t4T000001dMcG -w 20 -s AllUsers
 
 sfdx force:user:permset:assign -n  Inventory_Management_full
 
