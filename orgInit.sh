571271bed4eba1b331b22bbee208bc78ced7f6a3
Jaswinder Rattanpal <jrattanpal@gmail.com>
2019-10-04 14:27:02 -0400

    Fix issues with dashboard metadata, add Apex code to associate user to location, add namespace to sfdx-project.json file

--- orgInit.sh
+++ orgInit.sh
@@ -1,9 +1,11 @@
-sfdx force:org:create -a invmgrscratchorg1 -s -f config/project-scratch-def.json 
+sfdx force:org:create -a invmgrscratchorg1 -s -f config/project-scratch-def.json -d 30
 
 sfdx force:source:push
 
 sfdx force:user:permset:assign -n  Inventory_Management_full
 
 sfdx force:data:tree:import -p data/Plan.json
+
+sfdx force:apex:execute -f config/create-demo-data-setup.apex
 
 sfdx force:org:open -p /lightning/o/invmgrnp__Location__c/list
