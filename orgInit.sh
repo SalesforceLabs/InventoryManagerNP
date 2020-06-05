d08f833beea906dad2725a2309e45c519f753b39
Jaswinder Rattanpal <jrattanpal@gmail.com>
2020-06-05 12:15:32 -0400

    Update config file for new changes

--- orgInit.sh
+++ orgInit.sh
@@ -13,6 +13,6 @@ sfdx force:org:open -p /lightning/o/invmgrnp__Location__c/list
 
 #Deploy to Dev Org: Use -c to check only first
 #If in MDAPI format
-#sfdx force:mdapi:deploy -d src -u invmgrpkgorg -w 100 
+#sfdx force:source:deploy -m LightningComponentBundle -u invmgrpkgorg -w 100 
 #If in SFDX format
 #sfdx force:source:deploy -u invmgrpkgorg -p "force-app/main/default/lwc,force-app/main/default/reports,force-app/main/default/objects/Item__c/fields"
