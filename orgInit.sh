6ee9ba0803a7c7242e4db950e0f47b82a3510a58
Jaswinder Rattanpal <jrattanpal@gmail.com>
2021-11-30 10:13:10 -0500

    update

--- orgInit.sh
+++ orgInit.sh
@@ -1,11 +1,11 @@
 sfdx force:org:create -a invmgrscratchorgpkgtest -s -f config/project-scratch-def.json -d 7
 
-#v1.8
-sfdx force:package:install -p 04t4T000001dPrK -w 20
+#v1.9
+sfdx force:package:install -p 04t4T000001dQHA -w 20
 
 sfdx force:user:permset:assign -n  Inventory_Management_full
 
-sfdx force:data:tree:import -p data/Plan.json
+sfdx force:data:tree:import -p data_invmgrnp/Plan.json
 
 sfdx force:apex:execute -f config/create-demo-data-setup.apex
 
