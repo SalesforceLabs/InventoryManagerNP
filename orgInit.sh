86f81833fbd33238c7b8d685df2a2505abc1a4b0
Jaswinder Rattanpal <jrattanpal@gmail.com>
2023-06-14 14:25:11 +0530

    update API to 58.0 Update source code format to 58.0. However, this is not added to the new managed package yet. Because that is still working.

--- orgInit.sh
+++ orgInit.sh
@@ -1,14 +1,15 @@
-sfdx force:org:create -a invmgrscratchorg1 -s -f config/project-scratch-def.json -d 30
+sfdx org:create:scratch -a invmgrscratchorg1 -d -f config/project-scratch-def.json -y 30
 
-sfdx force:source:push
+#sfdx force:source:push
+sfdx project deploy start
 
 sfdx force:user:permset:assign -n  Inventory_Management_full
 
-sfdx force:data:tree:import -p data_invmgrnp/Plan.json
+sfdx data tree import -p data_invmgrnp/Plan.json
 
-sfdx force:apex:execute -f config/create-demo-data-setup.apex
+sfdx apex run -f config/create-demo-data-setup.apex
 
-sfdx force:org:open -p /lightning/o/invmgrnp__Location__c/list
+sfdx org open -p /lightning/o/invmgrnp__Location__c/list
 
 
 #Deploy to Dev Org: Use -c to check only first
